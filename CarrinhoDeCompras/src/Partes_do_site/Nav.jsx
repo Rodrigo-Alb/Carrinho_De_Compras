2665e757780f2e571d175cba3d8022707496af9c
Rodrigo da Costa <154638151+Rodrigo-Alb@users.noreply.github.com>
2024-05-23 14:02:49 -0300

    Alterando biblitoeca do Icon para deploy

--- CarrinhoDeCompras/src/Partes_do_site/Nav.jsx
+++ CarrinhoDeCompras/src/Partes_do_site/Nav.jsx
@@ -1,7 +1,7 @@
 import './Nav.css'
 import PropTypes from 'prop-types';
 
-import { FaCartShopping } from 'react-icons/fa6'
+import { BsFillCartFill } from "react-icons/bs";
 import { FaHamburger } from 'react-icons/fa'
 import { RxHamburgerMenu } from 'react-icons/rx'
 
@@ -15,7 +15,7 @@ export default function Nav(props) {
       <Button text={<RxHamburgerMenu/>} color='white' backgroundColor = '#9da88f' border='none'></Button>
       <Input />
       <Button text ={<FaHamburger />} color='black' backgroundColor='#e8efed' border='2px none ' />
-      <Button text = {<FaCartShopping/>} color='#e8efed' backgroundColor='white' border='2px solid #e8efed ' abrirCarrinho={props.abrirCarrinho} calcValor = {props.calcValor} />
+      <Button text = {<BsFillCartFill/>} color='#e8efed' backgroundColor='white' border='2px solid #e8efed ' abrirCarrinho={props.abrirCarrinho} calcValor = {props.calcValor} />
     </section> 
   )
 }
